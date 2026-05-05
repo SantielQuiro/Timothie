50d366a32e307d4e5c61d783b6f293615579d0e8
SantielQuiro <sqbmb130@gmail.com>
2026-05-05 21:37:01 +0200

    Maze Rotation with Mouse
    +Mouse Input with the new input system
    +ball and 4 walls
    +max rotation clamp commented

--- Assets/Scripts/MazeRoll Scripts/MouseInput.cs
+++ Assets/Scripts/MazeRoll Scripts/MouseInput.cs
@@ -1,9 +1,41 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class MouseInput : MonoBehaviour
 {
+    [SerializeField] Rigidbody rb;
+    [SerializeField] float rotationSpeed;
+    [SerializeField] float maxRotationAngle;
+    [SerializeField] float minRotationAngle;
+
+    public Vector3 mousePosition;
+    public InputActionReference deltaMouse;
+    bool OnMinigame;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        OnMinigame = true; 
+    }
+
     private void Update()
     {
-        Debug.Log("Mouse Position: " + Input.mousePosition); 
+        Debug.Log("Mouse Position: " + mousePosition + " and " + OnMinigame); 
+        mousePosition = deltaMouse.action.ReadValue<Vector2>();
+
+        if (OnMinigame)
+        {
+            Cursor.lockState = CursorLockMode.Locked; // Lock the cursor to the game window
+            //Cursor.visible = false; // Hide the cursor
+        }
+    }
+    private void FixedUpdate()
+    {
+        rb.AddTorque(new Vector3(mousePosition.y, 0, -mousePosition.x) * rotationSpeed * Time.fixedDeltaTime);
+        /*rb.rotation = Quaternion.Euler(
+            Mathf.Clamp(rb.rotation.eulerAngles.x, minRotationAngle, maxRotationAngle),
+            rb.rotation.eulerAngles.y,
+            Mathf.Clamp(rb.rotation.eulerAngles.z, minRotationAngle, maxRotationAngle)
+        );*/
     }
 }
